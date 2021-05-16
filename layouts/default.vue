893dfe061a61609672cb3b5ada8770c5ac36722a
jofe12 <jofe12@hotmail.com>
2021-05-16 18:29:28 -0500

    tercer service

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
     <side-bar
       :background-color="sidebarBackground"
       short-title="oneline"
-      title="jfda.oneline"
+      title="193.46.199.60"
     >
       <template slot-scope="props" slot="links">
         <sidebar-item
